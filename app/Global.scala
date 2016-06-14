fd13f9523021d3bc00b5bdee55cb47d4dcbe1c3c
aloise <aloise@aloise.name>
2016-06-14 19:37:50 +0300

    [+] 0.2h - project cleanup

--- app/Global.scala
+++ app/Global.scala
@@ -7,19 +7,11 @@ import akka.actor.Props
 import play.api._
 import play.api.libs.Crypto
 import play.api.mvc._
-import play.filters.gzip.GzipFilter
 import play.libs.Akka
 import play.modules.reactivemongo.ReactiveMongoPlugin
 import play.api.libs.concurrent.Akka
 
-
-object GlobalHelpers {
-  def shouldGzip(request:RequestHeader, response:ResponseHeader) = {
-    response.headers.get("Content-Type").exists(_.startsWith("text/javascript"))
-  }
-}
-
-object Global extends WithFilters(new GzipFilter( shouldGzip = GlobalHelpers.shouldGzip )) with GlobalSettings  {
+object Global extends WithFilters with GlobalSettings  {
 
   override def onStart(app: Application): Unit = {
 
