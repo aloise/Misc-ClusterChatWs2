367555712e46619c1515d9b3b93ce4aa61cddffc
aloise <aloise@aloise.name>
2016-06-16 18:10:24 +0300

    [+] 0h - admin - api - cross-browser settings - update

--- app/controllers/helpers/HeaderHelpers.scala
+++ app/controllers/helpers/HeaderHelpers.scala
@@ -6,9 +6,7 @@ package controllers.helpers
 object HeaderHelpers {
 
   val crossOriginHeaders = Seq[(String,String)](
-    "Access-Control-Allow-Origin" -> "*",
     "Access-Control-Allow-Origin" -> play.api.Play.current.configuration.getString("assistants.domain").getOrElse("*"),
-    "Allow" -> "*",
     "Access-Control-Allow-Methods" -> "POST, GET, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Credentials" -> "true",
     "Access-Control-Allow-Headers" -> "Origin, X-Requested-With, Content-Type, Accept, Referrer, User-Agent, Cookie, X-Json, X-Prototype-Version",
